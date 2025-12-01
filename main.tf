303689fa7c209cbd2a2efc88e71e0e1460372bc5
Anujkr-devOps <L00188535@atu.ie>
2025-12-01 23:12:03 +0000

    updated main.tf with VPC and SUbnet

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
 #Main resources (VPC,EC2,etc)
+
+#VPC Resource
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "TerraformAssignmentVPC"
+  }
+}
+
+#Public Subnet Resource in AZ1
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet-AZ1"
+  }
+}
+
+#Public Subnet Resource in AZ2
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet-AZ2"
+  }
+}
