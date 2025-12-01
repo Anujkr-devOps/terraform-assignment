0b02e50d6a58199ca6286b04166690ef159f1a0b
Anujkr-devOps <L00188535@atu.ie>
2025-12-01 23:21:45 +0000

    added internet gateway and Route table

--- main.tf
+++ main.tf
@@ -29,3 +29,39 @@ resource "aws_subnet" "public_subnet_az2" {
     Name = "PublicSubnet-AZ2"
   }
 }
+
+
+#Internet Gateway Resource
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "TerraformAssignmentIGW"
+  }
+}
+
+#Public Route Table Resource
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+#Route to Internet
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+#Associate Route Table with Public Subnet AZ1
+resource "aws_route_table_association" "public_subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+#Associate Route Table with Public Subnet AZ2
+resource "aws_route_table_association" "public_subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
