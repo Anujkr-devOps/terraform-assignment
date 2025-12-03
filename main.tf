6d54b3f2f49d4107fb14e8bd54584b061127dded
Anujkr-devOps <L00188535@atu.ie>
2025-12-03 00:24:49 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-#Main resources (VPC,EC2,etc)
-
-#VPC Resource
+# VPC Resource
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -8,30 +6,29 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-#Public Subnet Resource in AZ1
+# Public Subnet Resource in AZ1
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-1a"
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet-AZ1"
   }
 }
 
-#Public Subnet Resource in AZ2
+# Public Subnet Resource in AZ2
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-west-1b"
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet-AZ2"
   }
 }
 
-
-#Internet Gateway Resource
+# Internet Gateway Resource
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
@@ -39,7 +36,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-#Public Route Table Resource
+# Public Route Table Resource
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
@@ -47,22 +44,22 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-#Route to Internet
+# Route to Internet
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-#Associate Route Table with Public Subnet AZ1
+# Associate Route Table with Public Subnet AZ1
 resource "aws_route_table_association" "public_subnet_1_assoc" {
-  subnet_id      = aws_subnet.public_subnet_1.id
+  subnet_id      = aws_subnet.public_subnet_az1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-#Associate Route Table with Public Subnet AZ2
+# Associate Route Table with Public Subnet AZ2
 resource "aws_route_table_association" "public_subnet_2_assoc" {
-  subnet_id      = aws_subnet.public_subnet_2.id
+  subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
@@ -85,7 +82,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_PUBLIC_IP/32"] # Replace with your IP for security
+    cidr_blocks = ["<YOUR_PUBLIC_IP>/32"] # Replace with your actual public IP
   }
 
   egress {
@@ -102,11 +99,12 @@ resource "aws_security_group" "web_sg" {
 
 # EC2 Instance in AZ1
 resource "aws_instance" "web_server_1" {
-  ami           = "ami-0c02fb55956c7d316" #Amazon Linux 2 AMI 
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet_1.id
+  subnet_id     = aws_subnet.public_subnet_az1.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name      = "firstkeypair" 
+  key_name      = "firstkeypair"
+
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
@@ -125,7 +123,7 @@ resource "aws_instance" "web_server_1" {
 resource "aws_instance" "web_server_2" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet_2.id
+  subnet_id     = aws_subnet.public_subnet_az2.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name      = "firstkeypair"
 
@@ -143,23 +141,7 @@ resource "aws_instance" "web_server_2" {
   }
 }
 
-# Elastic IP for Web Server 1
-resource "aws_eip" "eip_1" {
-  instance = aws_instance.web_server_1.id
-  tags = {
-    Name = "ElasticIP-WebServer1"
-  }
-}
-
-# Elastic IP for Web Server 2
-resource "aws_eip" "eip_2" {
-  instance = aws_instance.web_server_2.id
-  tags = {
-    Name = "ElasticIP-WebServer2"
-  }
-}
-
-#ADD ALB security group 
+# ALB Security Group
 resource "aws_security_group" "alb_sg" {
   name        = "alb-sg"
   description = "Allow HTTP traffic to ALB"
@@ -185,7 +167,7 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-#creating ALB
+# Application Load Balancer
 resource "aws_lb" "app_lb" {
   name               = "terraform-assignment-alb"
   internal           = false
@@ -232,7 +214,7 @@ resource "aws_lb_listener" "app_listener" {
   }
 }
 
-#Attach EC2 instances to Target Group
+# Attach EC2 instances to Target Group
 resource "aws_lb_target_group_attachment" "web_server_1_attach" {
   target_group_arn = aws_lb_target_group.app_tg.arn
   target_id        = aws_instance.web_server_1.id
@@ -245,7 +227,7 @@ resource "aws_lb_target_group_attachment" "web_server_2_attach" {
   port             = 80
 }
 
-#output ALB DNS name
+# Output ALB DNS name
 output "alb_dns_name" {
   value = aws_lb.app_lb.dns_name
 }
