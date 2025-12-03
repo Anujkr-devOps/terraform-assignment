b3205d55c597105873a80dabf150ec13d46b9713
Anujkr-devOps <L00188535@atu.ie>
2025-12-03 00:07:42 +0000

    added ALB integration

--- main.tf
+++ main.tf
@@ -158,3 +158,94 @@ resource "aws_eip" "eip_2" {
     Name = "ElasticIP-WebServer2"
   }
 }
+
+#ADD ALB security group 
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALB-SG"
+  }
+}
+
+#creating ALB
+resource "aws_lb" "app_lb" {
+  name               = "terraform-assignment-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_az1.id, aws_subnet.public_subnet_az2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "TerraformAssignmentALB"
+  }
+}
+
+# Target Group for ALB
+resource "aws_lb_target_group" "app_tg" {
+  name     = "terraform-assignment-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "TerraformAssignmentTG"
+  }
+}
+
+# Listener for ALB
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+#Attach EC2 instances to Target Group
+resource "aws_lb_target_group_attachment" "web_server_1_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_server_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web_server_2_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_server_2.id
+  port             = 80
+}
+
+#output ALB DNS name
+output "alb_dns_name" {
+  value = aws_lb.app_lb.dns_name
+}
